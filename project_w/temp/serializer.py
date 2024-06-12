ec8c4a30ce3dcf4a39398cce30954d18161bd69e
Jacob Seol <seouljacob@meta.com>
2024-06-12 14:52:39 -0700

    Initial commit

--- project_w/temp/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class BookSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    publication_date = serializers.DateField()
